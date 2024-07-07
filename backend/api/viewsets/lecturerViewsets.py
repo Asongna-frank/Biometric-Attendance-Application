8efa7ec9d5d069bad9b0e9b9dc60392042fa16cc
Asongna-frank <87443470+Asongna-frank@users.noreply.github.com>
2024-07-07 22:14:46 +0100

    added full CRUD functionality to lecturer and student API

--- backend/api/viewsets/lecturerViewsets.py
+++ backend/api/viewsets/lecturerViewsets.py
@@ -1,20 +1,34 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from api.serializers.lecturer import LecturerSerializer
 from bioattend.models import Lecturer
 
 
 class LecturerListViewset(viewsets.ModelViewSet):
-    http_method_names = ['get', 'post']
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
 
     serializer_class = LecturerSerializer
 
     queryset = Lecturer.objects.all()
 
+    def put(self, request, pk=None):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=False)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
-class LecturerListViewset(viewsets.ModelViewSet):
-    http_method_names = ['get', 'post']
-    serializer_class = LecturerSerializer
-    queryset = Lecturer.objects.all()
+    def patch(self, request, pk=None):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, pk=None):
+        instance = self.get_object()
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     def get_queryset(self):
         queryset = self.queryset
--- backend/api/viewsets/studentViewsets.py
+++ backend/api/viewsets/studentViewsets.py
@@ -1,19 +1,34 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from api.serializers.student import StudentSerializer
 from bioattend.models import Student
 
 
 class StudentListViewset(viewsets.ModelViewSet):
-    http_method_names = ['get', 'post']
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
 
     serializer_class = StudentSerializer
 
     queryset = Student.objects.all()
 
+    def put(self, request, pk=None):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=False)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
-class StudentListViewset(viewsets.ModelViewSet):
-    http_method_names = ['get', 'post']
-    serializer_class = StudentSerializer
+    def patch(self, request, pk=None):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, pk=None):
+        instance = self.get_object()
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     def get_queryset(self):
         queryset = Student.objects.all()

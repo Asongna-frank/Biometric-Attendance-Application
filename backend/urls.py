bad301f267d576647d9e1e3cd9492d1dc39e2bb3
Asongna-frank <87443470+Asongna-frank@users.noreply.github.com>
2024-07-14 21:12:48 +0100

    commented urls.py file and reset token functions

--- backend/urls.py
+++ backend/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from users.custom_admin import custom_admin_site
-from users.views import ObtainTokenPairWithEmailView
-from rest_framework_simplejwt.views import TokenRefreshView
+# from users.views import ObtainTokenPairWithEmailView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', custom_admin_site.urls),
     path("api/", include("api.routes")),
-    path('api/token/', ObtainTokenPairWithEmailView.as_view(), name='token_obtain_pair'),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]

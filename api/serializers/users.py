9e7c15f0ce0e786a03a403a439de0af8834ff136
Asongna-frank <87443470+Asongna-frank@users.noreply.github.com>
2024-07-15 12:52:46 +0100

    updated the user serializer file

--- api/serializers/users.py
+++ api/serializers/users.py
@@ -3,16 +3,26 @@ from rest_framework import serializers
 from users.models import User
 from django.contrib.auth.models import update_last_login
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from bioattend.models import Student, Lecturer
 
 class UserSerializer(serializers.ModelSerializer):
+    role = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ['public_id', 'user_name', 'email', 'number']
+        fields = ['public_id', 'user_name', 'email', 'number', 'role']
+
+    def get_role(self, obj):
+        if Student.objects.filter(user=obj).exists():
+            return 'student'
+        elif Lecturer.objects.filter(user=obj).exists():
+            return 'teacher'
+        return 'unknown'
 
 class LoginSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
-        refresh = self.get_token(user=self.user)
+        refresh = self.get_token(self.user)
         user = UserSerializer(self.user).data
         data['refresh'] = str(refresh)
         data['access'] = str(refresh.access_token)
